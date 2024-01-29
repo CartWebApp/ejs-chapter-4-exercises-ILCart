c480f5b501f5a63d3ad0f8d2eb8b8e998425d8b9
ILCart <isaac.lay23@student.cart.org>
2024-01-29 10:43:23 -0800

    Update deep-equal.js

--- deep-equal.js
+++ deep-equal.js
@@ -1,5 +1,11 @@
 function deepEqual(value, reference) {
-  // Add code.
+  if(value === reference){
+    return true;
+  }
+  for(const [key, entry] of Object.entries(reference)){
+    console.log(value[key] == entry,value[key],entry,"e");
+  }
+  return true;
 }
 
 
@@ -9,9 +15,9 @@ function deepEqual(value, reference) {
 
 // tests
 let obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
+// console.log(deepEqual(obj, obj));
+// // → true
+// console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
